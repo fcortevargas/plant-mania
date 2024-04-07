1019186fa52324c08754c0d5188b1ab7ec46bdfe
fcortevargas <f.cortevargas@gmail.com>
2024-04-07 18:59:00 +0200

    :sparkles: Created API with endpoints to get all users, create a user, get an individual user and delete an user.

---
+++ app/api.py
@@ -0,0 +1,71 @@
+from http import HTTPStatus
+from typing import List
+
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+from app.database.models.user import User
+from app.dependencies import get_session
+from app.schemas.user import UserCreate, UserOut
+
+app = FastAPI()
+
+
+@app.get("/")
+def main():
+    return {"Hello": "World"}
+
+
+@app.get("/users", response_model=List[UserOut])
+def get_all_users(db_session: Session = Depends(get_session)):
+    users = db_session.query(User).all()
+    return users
+
+
+@app.post("/users", response_model=UserOut)
+def create_user(
+        user_create: UserCreate,
+        db_session: Session = Depends(get_session)
+):
+    new_user = User(**user_create.model_dump())
+
+    db_session.add(new_user)
+    db_session.commit()
+    db_session.refresh(new_user)
+
+    return new_user
+
+
+@app.get("/users/{user_id}", response_model=UserOut)
+def get_user(
+        user_id: int,
+        db_session: Session = Depends(get_session)
+):
+    user = db_session.query(User).filter(User.id == user_id).one_or_none()
+
+    if not user:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND,
+            detail=f"User with ID={user_id} not found."
+        )
+
+    return user
+
+
+@app.delete("/users/{user_id}", response_model=UserOut)
+def delete_user(
+        user_id: int,
+        db_session: Session = Depends(get_session)
+):
+    user = db_session.query(User).filter(User.id == user_id).one_or_none()
+
+    if not user:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND,
+            detail=f"User with ID={user_id} not found."
+        )
+
+    db_session.delete(user)
+    db_session.commit()
+
+    return user
---
+++ app/dependencies.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+STORAGE_URL = "postgresql://plants:pass@localhost/plants"
+
+
+def get_session():
+    engine = create_engine(STORAGE_URL)
+
+    session = scoped_session(sessionmaker(bind=engine))
+
+    try:
+        yield session
+    finally:
+        session.close()
