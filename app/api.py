9d78830f7e4ae09f3b00db44868bb4f6b9256d2d
fcortevargas <f.cortevargas@gmail.com>
2024-04-08 23:12:10 +0200

    :sparkles: Included routers of plants and species to the API.

--- app/api.py
+++ app/api.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
 
+from app.domains.plants.router import plant_router
+from app.domains.species.router import species_router
 from app.domains.users.router import user_router
 
 app = FastAPI()
 
 app.include_router(user_router)
+app.include_router(plant_router)
+app.include_router(species_router)
 
 
 @app.get("/")
